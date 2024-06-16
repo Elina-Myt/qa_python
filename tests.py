58803eab3a386827075daf46321c4e46944e6f2f
Elina Mityagina <mityaginaelina@yandex.ru>
2024-06-16 12:19:15 +0300

    покрытие тестами класса BooksCollector

--- tests.py
+++ tests.py
@@ -1,24 +1,87 @@
 from main import BooksCollector
+import pytest
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+
+    @pytest.mark.parametrize('name', ['Гордость и предубеждение и зомби', 'Что делать, если ваш кот хочет вас убить'])
+    def test_add_new_book_add_book(self, name):
         collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.add_new_book(name)
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.books_genre) == 1
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_set_book_genre_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('name1')
+        collector.set_book_genre('name1', 'Фантастика')
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        assert collector.books_genre['name1'] == 'Фантастика'
+
+    def test_set_book_genre_book_out_of_genre_list(self):
+        collector = BooksCollector()
+        collector.add_new_book('name1')
+        collector.set_book_genre('name1', 'Роман')
+
+        assert collector.books_genre['name1'] == ''
+
+    def test_get_book_genre_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.set_book_genre('Дюна', 'Фантастика')
+
+        assert collector.books_genre.get('Дюна') == 'Фантастика'
+
+    def test_get_books_with_specific_genre_book_list_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.add_new_book('1984')
+        collector.set_book_genre('Дюна', 'Фантастика')
+        collector.set_book_genre('1984', 'Фантастика')
+
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Дюна','1984']
+
+    def test_get_books_genre_book_dictionary(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.add_new_book('Дракула')
+        collector.set_book_genre('Дюна', 'Фантастика')
+        collector.set_book_genre('Дракула', 'Ужасы')
+
+        assert collector.get_books_genre() == {'Дюна':'Фантастика','Дракула':'Ужасы'}
+
+    def test_get_books_for_children_book_list(self):
+        collector = BooksCollector()
+        collector.add_new_book('Колобок')
+        collector.add_new_book('Дракула')
+        collector.set_book_genre('Колобок', 'Мультфильмы')
+        collector.set_book_genre('Дракула', 'Ужасы')
+
+        assert collector.get_books_for_children() == ['Колобок']
+
+    def test_add_book_in_favorites_book_list(self):
+        collector = BooksCollector()
+        collector.add_new_book('1984')
+        collector.add_book_in_favorites('1984')
+
+        assert collector.favorites == ['1984']
+
+    def test_delete_book_from_favorites_book_list(self):
+        collector = BooksCollector()
+        collector.add_new_book('1984')
+        collector.add_new_book('Колобок')
+        collector.add_book_in_favorites('1984')
+        collector.add_book_in_favorites('Колобок')
+        collector.delete_book_from_favorites('Колобок')
+
+        assert collector.favorites == ['1984']
+
+    def test_get_list_of_favorites_book_list(self):
+        collector = BooksCollector()
+        collector.add_new_book('Что делать?')
+        collector.add_book_in_favorites('Что делать?')
+
+        assert collector.get_list_of_favorites_books() == ['Что делать?']
